fcde7447d40382e33f94f17277c4e2969bebc7e7
rebuildup <rebuild.up.up@gmail.com>
2026-01-15 15:47:18 +0900

    fix(analytics): remove conflicting GTM scripts to ensure correct GA4 tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,17 +16,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
 		<html lang="ja" suppressHydrationWarning>
 			<head>
-				{/* Google Tag Manager */}
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MF58RWH6');`,
-					}}
-				/>
-				{/* End Google Tag Manager */}
 				{/* Google Fonts */}
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link
@@ -40,22 +29,13 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 				/>
 			</head>
 			<body className="bg-base text-main font-sans">
-				{/* Google Tag Manager (noscript) */}
-				<noscript>
-					<iframe
-						src="https://www.googletagmanager.com/ns.html?id=GTM-MF58RWH6"
-						height="0"
-						width="0"
-						style={{ display: "none", visibility: "hidden" }}
-					/>
-				</noscript>
-				{/* End Google Tag Manager (noscript) */}
 				<ProductionInitializer>
 					<AnalyticsProvider>
 						{children}
 						<CookieConsent />
 					</AnalyticsProvider>
 				</ProductionInitializer>
+
 				{/* React DevTools version fix */}
 				<Script
 					id="react-devtools-fix"
